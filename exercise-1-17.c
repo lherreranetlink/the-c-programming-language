bc5deb2cefb6d456c51b829fe03c6e67c6d799c1
Leonardo Herrera Ornelas <leonardoherreraornelas@Leonardos-MacBook-Pro.local>
2025-03-25 21:53:37 -0600

    added Exercise-1-17.c

---
+++ exercise-1-17.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+#define MIN_LINE_SIZE 80
+
+int getlinecustom(char line[], int maxline);
+void printline(char line[], int size);
+
+int main()
+{
+    int lineSize;
+    char line[MAXLINE];
+
+    while ((lineSize = getlinecustom(line, MAXLINE)) > 0)
+        if (lineSize >= MIN_LINE_SIZE)
+            printline(line, lineSize);
+
+    return 0;
+}
+
+int getlinecustom(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+
+    return i;
+}
+
+void printline(char line[], int size)
+{
+    int i, c;
+    for (i = 0; i < size; ++i)
+        putchar(line[i]);
+}

eceef7b526a2035718f3eab8b20dd263804f416e
Leonardo Herrera Ornelas <leonardoherreraornelas@Leonardos-MacBook-Pro.local>
2025-06-04 01:19:23 -0600

    exercise-2-5.c approach completed

--- exercise-2-6.c
+++ exercise-2-6.c
@@ -6,26 +6,43 @@ unsigned getrightmostbits(unsigned y, int n);
 
 int main()
 {
-
-    unsigned number = 1000;
+    unsigned x = 3538701066;
+    unsigned y = 3538701066;
+    unsigned result = 0;
     int n = 5;
-    int y = 8;
-    unsigned nbitsfromp = getbits(number, 9, n);
-    unsigned righmostnfromy = getrightmostbits(y, n);
+    int p = 21;
 
-    unsigned result = number & ((righmostnfromy << n) | ~(~0 << n)); 
+    int provisional1 = x >> (p + 1); 
 
-    printf("Resultado locochon: %u\n", result);
+    provisional1 = provisional1 << (p + 1);
 
-    //printf("Resultado locochon: %u\n", test2);
+    int provisional2 = getrightmostbits(x, (p + 1 - n));
 
-    return 0;
+    printf("provisional1: %u\n", provisional1);
+    printf("provisional1: %u\n", provisional2);
+
+    unsigned provisionalResult1 = provisional1 | provisional2;
+
+    printf("provisionalResult1: %u\n", provisionalResult1);
+
+    unsigned provisional3 = getrightmostbits(y, n);
+
+    printf("provisional3: %u\n", provisional3);
+
+    unsigned provisionalResult2 = provisional3 << (p + 1 - n);
+
+    printf("provisionalResult2: %u\n", provisionalResult2);
+
+    unsigned finalResult = provisionalResult1 | provisionalResult2;
+
+    printf("finalResult: %u\n", finalResult);
+
 }
 
 //get n number of bits starting from position p from an unsigned x leaving the rest of the bits unchanged
 unsigned getbits(unsigned x, int p, int n)
 {
-    return ((x >> (p + 1 - n)) & ~(~0 << n)) << (p + 1 - n);
+    return ((x >> (p + 1 - n)) & ~(~0 << n));
 }
 
 //get n rightmost bits from unsigned y
